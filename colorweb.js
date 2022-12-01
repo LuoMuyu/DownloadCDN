946989dc2ccf57027ffc1cca359297e5f3dbb43e
LuoMuyu <47489339+LuoMuyu@users.noreply.github.com>
2022-12-01 11:50:51 +0800

    Create colorweb.js

---
+++ colorweb.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         还我彩色
+// @version      0.1
+// @description  Color back
+// @author       洛沐语
+// @match        http*://*/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    GM_addStyle("* {filter: none !important;}");
+})();
